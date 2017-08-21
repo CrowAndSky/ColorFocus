b27ef6ec9fa1734a03e53d38ee539b9153a9affb
Preston Williams <prwilliams@sherwin.com>
2017-08-21 16:48:17 -0400

    Multiple updates

--- app.js
+++ app.js
@@ -1,49 +1,48 @@
 "use strict";
 
+/*\
+|*|
+|*|  :: wwwwwwwwww ::
+|*|
+|*|  :: wwwwwwwwww ::
+TO DO:
+Add global vars for colors and write them to CSS
+
+Add choice and slide show controls and style
+\*/
+
  /* -------------------- INIT VARIABLES ---------------------*/
 var /*--------------------- ### DOM elements ### ---------------------*/
-$appWrapper = $( '#perfect-app-wrapper' ),
-colorDetails = $( '.color__details' ),
+$appWrapper = $( '#app-wrapper' ),
+$compareLink = $( '.navigation__compare' ),
+$favoritesLink = $( '.navigation__favorites' ),
+$introReadyCTA = $( '.intro__message .button-link' ),
+$introColorMasks = $( '.intro-pane-color-mask' ),
+$introColorsHue = $( '.intro__hue .intro-pane-color-mask' ),
+$introColorsLuminosity = $( '.intro__luminosity .intro-pane-color-mask' ),
+$introColorsSaturation = $( '.intro__saturation .intro-pane-color-mask' ),
+$roomB = $( '.room-b' ),
+$roomLower = $( '.room__wrapper--lower' ),
+$roomLowerB = $( '.room__wrapper--lower.room-b' ),
+$introColor = $( '.wwwwwww' ),
+$introColor = $( '.wwwwwww' ),
+
 chipStyleSheet = document.styleSheets[0],
 
 
-/*--------------------- ### wwwwwww ### ---------------------
-IDEAS:
-Slide slow mdoe uses the same layers (lower room and details) with the addition of the controller
-
-Pro version is the same app with the addition of the color picker layer - start working on UI for that while coding main app logic?
-
-
-TO DO:
-
-
-1, 132, 152
-90, 158, 192
-
-97, 168, 157
-151, 203, 210 SW 9046 Gentle Aquamarine
-
-132, 180, 190 SW 6493 Ebbtide
-153, 160, 178 SW 6542 Vesper Violet
-
-191, 210, 201
-110, 194, 196  SW 6766 Mariner
-
-
-var styles = getComputedStyle(document.documentElement);
-var colorValue = styles.getPropertyValue('--color');
-document.documentElement.style.setProperty('--color', 'green');
-OR
-body.style.setProperty('--foo-bar', newValue);
-var fooBar = bodyStyles.getPropertyValue('--foo-bar');
-
-
-will-change: transform, opacity;
-*/
+$color-family-red: #ff0000;
+$color-family-orange: #FFA500;
+$color-family-yellow: #FFFF00;
+$color-family-green: #339935;
+$color-family-blue: #4D4AFD;
+$color-family-purple: #800080;
+$color-family-white: #F5F5F5;
+$color-family-neutral: #B9A796;
+$color-family-historic: #C08F80;
+$color-family-timeless: #703229;
 
 /*--------------------- ### App State ### ---------------------*/
-inDetailView = false,
-tempColors = ['#34ac67','#34ac67'],
+appState,    // state-intro    state-intro-hue   state-lower-room  state-color-preference   state-detail  state-transition state-app-primed
 
 /*--------------------- ### Animation Looping ### ---------------------*/
 animLoopIndex,
@@ -72,8 +71,27 @@ var funfunfun = function( event ) {
     }
 };
 
+
+// state-lower-room state-detail state-transition">
 /* ------------------ ### initDOM ### ------------------ */
 var initDOM = function( event ) {
+    $introReadyCTA.click( function(){ $appWrapper.class( 'state-intro-hue' ); } );
+
+    introColorsHue.click( function(){
+        $appWrapper.class( 'state-intro-luminosity' );
+    } );
+
+    introColorsLuminosity.click( function(){
+        $appWrapper.class( 'state-intro-saturation' );
+    } );
+
+    introColorsSaturation.click( function(){
+        $appWrapper.class( 'xxxxxx' );
+    } );
+
+    wwwwwwww.click( function(){ $appWrapper.class( 'xxxxxxxx' ) } );
+    wwwwwwww.click( function(){ $appWrapper.class( 'xxxxxxxx' ) } );
+    wwwwwwww.click( function(){ $appWrapper.class( 'xxxxxxxx' ) } );
     return true;
 };
 
@@ -133,9 +151,9 @@ $( '.app-title' ).click( function(){
     $appWrapper.toggleClass( 'state-lower-room' );
 });
 
-$( '.intro-pane-color-mask' ).click( function(){
-    $appWrapper.toggleClass( 'state-test1' );
-});
+// $( '.intro-pane-color-mask' ).click( function(){
+//     $appWrapper.toggleClass( 'state-test1' );
+// });
 
 window.setTimeout( function(){
     $appWrapper.addClass( 'state-app-primed state-detail-' );
@@ -144,6 +162,8 @@ window.setTimeout( function(){
 $( window ).resize(function() {
   handleResize();
 });
+
+//class="state-intro-hue- state-lower-room- state-intro- state-color-preference state-zoom-room- state-detail- state-transition">
 
 initDOM();
 handleResize();
@@ -181,4 +201,39 @@ temp color pairings
 191, 210, 201
 110, 194, 196  SW 6766 Mariner
 
+*/
+
+
+/*--------------------- ### wwwwwww ### ---------------------
+IDEAS:
+Slide slow mdoe uses the same layers (lower room and details) with the addition of the controller
+
+Pro version is the same app with the addition of the color picker layer - start working on UI for that while coding main app logic?
+
+
+TO DO:
+
+
+1, 132, 152
+90, 158, 192
+
+97, 168, 157
+151, 203, 210 SW 9046 Gentle Aquamarine
+
+132, 180, 190 SW 6493 Ebbtide
+153, 160, 178 SW 6542 Vesper Violet
+
+191, 210, 201
+110, 194, 196  SW 6766 Mariner
+
+
+var styles = getComputedStyle(document.documentElement);
+var colorValue = styles.getPropertyValue('--color');
+document.documentElement.style.setProperty('--color', 'green');
+OR
+body.style.setProperty('--foo-bar', newValue);
+var fooBar = bodyStyles.getPropertyValue('--foo-bar');
+
+
+will-change: transform, opacity;
 */
