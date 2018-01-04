a7e51dafbe81254abb2e689ca8c09c49208b13db
Preston Williams <prwilliams@sherwin.com>
2018-01-03 19:55:34 -0500

    Created an S-W version and started an React experiment

---
+++ app-r.js
@@ -0,0 +1,26 @@
+"use strict";
+
+/*\
+
+|*|  :: wwwwwwwwww ::
+|*|       TO DO:
+|*|  :: wwwwwwwwww ::
+
+Color-info stuff:
+    Animate gradient fill in diagonally 
+    Set max size for pairs of color names so that they are the same
+        And, if there are three words, shrink it more
+
+Scene digest:
+    Create animation on current colors
+    When choosing is ready, create faux series of clicks?
+\*/
+
+ /* -------------------- wwwwwwwwwwwww ---------------------*/
+ /* -------------------- wwwwwwwwwwwww ---------------------*/
+ /* -------------------- wwwwwwwwwwwww ---------------------*/
+ /* -------------------- wwwwwwwwwwwww ---------------------*/
+ /* -------------------- wwwwwwwwwwwww ---------------------*/
+
+
+ 
